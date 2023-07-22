e6c4400c05ba50cfa5bf8457110d7b2b63214f2a
Sam <57677512+threesam@users.noreply.github.com>
2023-07-22 13:57:23 -0400

    updates deps, adds media and vision plugins

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
-import { schemaTypes } from './schemas'
 import Logo from './components/Logo'
+import { media } from 'sanity-plugin-media'
+import { schemaTypes } from './schemas'
+import { visionTool } from '@sanity/vision'
 
 export default defineConfig({
   name: 'ArtKillingApathy',
   title: 'Art Killing Apathy',
   projectId: 'ppo5s5uj',
   dataset: 'production',
-  plugins: [deskTool()],
+  plugins: [deskTool(), media(), visionTool()],
   schema: {
     types: schemaTypes,
   },
